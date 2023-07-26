6ad178f28141f56f60b97c6e22239d5e262bcd27
Ninamahdawe <ninamahdawe11@gmail.com>
2023-07-26 11:43:55 -0700

    added the script.js var's

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
-"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+
+    var cityInput = document.getElementById("cityinput");
+    var searchButton = document.querySelector("button");
+    var clearButton = document.getElementById("clear-button");
+    var pastCitiesButtons = document.querySelectorAll("#cities-list");
+
+    searchButton.
+
+
+
+});
+
+
